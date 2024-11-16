f9027be2221af25b6e9fb54bfb535c4bb7cb2225
JordanYanakiev <jordan.yanakiev@gmail.com>
2024-11-16 22:35:11 +0200

    Made the logic to detect diagonals now have to identify five consecutive tiles

--- TicTacToe_2021/Assets/Scripts/MakeSquareRed.cs
+++ TicTacToe_2021/Assets/Scripts/MakeSquareRed.cs
@@ -28,7 +28,10 @@ public class MakeSquareRed : MonoBehaviour
         }
 
         PopulateField.instance.CheckWinCondition(xPos, yPos, player);
+
+        //Chech both diagonals for 5 consecutive tiles
         CheckDiagonalRight();
+        CheckDiagonalLeft();
     }
 
     private void CheckDiagonalRight()
@@ -45,10 +48,11 @@ public class MakeSquareRed : MonoBehaviour
         {
             while (x >= 0)
             {
-                
                 temp = gameFieldPanel.GetComponent<PopulateField>().GameField[x, y];
                 temp.GetComponent<SpriteRenderer>().color = Color.cyan;
-                Debug.Log(x + " " + y);
+                if (temp.tag == "Player")
+                    temp.GetComponent<SpriteRenderer>().color = Color.yellow;
+                //Debug.Log(x + " " + y);
                 x--;
                 y++;
             }
@@ -56,8 +60,84 @@ public class MakeSquareRed : MonoBehaviour
             xStart = x;
             y = 0;
         }
+
+
+        x = 8;
+        xStart = x;
+        yStart = 1;
+        y = yStart;
+        int end = 1;
+
+        while (y < 5)
+        {
+            while (x >= end)
+            {
+                temp = gameFieldPanel.GetComponent<PopulateField>().GameField[x, y];
+                temp.GetComponent<SpriteRenderer>().color = Color.cyan;
+                if (temp.tag == "Player")
+                    temp.GetComponent<SpriteRenderer>().color = Color.yellow;
+                //Debug.Log(x + " " + y);
+                x--;
+                y++;
+            }
+            end++;
+            x = 8;
+            y = yStart + 1;
+            yStart = y;
+        }
+
     }
 
+    private void CheckDiagonalLeft()
+    {
+        GameObject tile = gameFieldPanel.GetComponent<PopulateField>().GameField[0, 4];
+        tile.GetComponent<SpriteRenderer>().color = Color.blue;
+        GameObject temp = null;
+        int x = 0;
+        int xStart = x;
+        int y = 4;
+        int yStart = y;
 
+
+        int end = 4;
+        while (y >= 0)
+        {
+            while (x <= end)
+            {
+                temp = gameFieldPanel.GetComponent<PopulateField>().GameField[x, y];
+                temp.GetComponent<SpriteRenderer>().color = Color.blue;
+                if (temp.tag == "Player")
+                    temp.GetComponent<SpriteRenderer>().color = Color.yellow;
+                //Debug.Log(x + " BLUE " + y);
+                x++;
+                y++;
+            }
+            end++;
+            x = 0;
+            y = yStart - 1;
+            yStart = y;
+        }
+
+
+        x = 1;
+        xStart = x;
+        y = 0;
+        while (x <= 4)
+        {
+            while (x <= 8)
+            {
+                temp = gameFieldPanel.GetComponent<PopulateField>().GameField[x, y];
+                temp.GetComponent<SpriteRenderer>().color = Color.blue;
+                if (temp.tag == "Player")
+                    temp.GetComponent<SpriteRenderer>().color = Color.yellow;
+                //Debug.Log(x + " " + y);
+                x++;
+                y++;
+            }
+            x = xStart + 1;
+            xStart = x;
+            y = 0;
+        }
+    }
 
 }
